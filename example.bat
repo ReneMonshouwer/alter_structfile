ae3fa22cf44e48020e54d05df3206624bdec2541
Rene Monshouwer <96694959+ReneMonshouwer@users.noreply.github.com>
2022-01-23 18:29:18 +0100

    Update example.bat

--- example.bat
+++ example.bat
@@ -1,3 +1,3 @@
-alter_structfile.exe -l **/*.dcm LiLo L_Lung ReLo R_Lung WH Heart Myelum Myelum myelum Myelum Cord3 Myelum Cord1 Myelum wervelkanaal Myelum ToLo Lungs Oes_Ext_RoWi Esophagus
-alter_structfile.exe -r **/*.dcm LiLo L_Lung ReLo R_Lung WH Heart Myelum Myelum myelum Myelum Cord3 Myelum Cord1 Myelum wervelkanaal Myelum ToLo Lungs Oes_Ext_RoWi Esophagus
+alter_structfile.exe -l **/*.dcm LiLo L_Lung ReLo R_Lung WH Heart Myelum Myelum myelum Myelum Cord3 Myelum Cord1 Myelum wervelkanaal Myelum ToLo Lungs Oes_Ext Esophagus
+alter_structfile.exe -r **/*.dcm LiLo L_Lung ReLo R_Lung WH Heart Myelum Myelum myelum Myelum Cord3 Myelum Cord1 Myelum wervelkanaal Myelum ToLo Lungs Oes_Ext Esophagus
 alter_structfile.exe -l **/*.dcm L_Lung R_Lung Heart Myelum Lungs Esophagus 
